5da1985e0f7441e877214c3d97c5c2af5f514511
Sauli Sillanpää <sauli.sillanpaa@gmail.com>
2026-03-11 14:14:22 +0200

    3.12.

---
+++ mongo.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://saulisillanpaa_db_user:${password}@cluster0.lpdnuru.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+    })
+} else if (process.argv.length == 3) {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+/*
+const person = new Person({
+    
+    name: "Arto Hellas", 
+    number: "040-123456",
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
+*/
