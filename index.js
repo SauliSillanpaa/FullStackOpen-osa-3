eb27fde0518e908cb5041a80401a51f521795471
Sauli Sillanpää <sauli.sillanpaa@gmail.com>
2026-03-11 19:08:57 +0200

    3.15.-3.16.

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 app.use(morgan('tiny'))
 app.use(cors())
 
@@ -47,6 +66,19 @@ app.get('/info', (request, response) => {
     `)
 })
 
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+/*
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find(person => person.id === id)
@@ -57,13 +89,24 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+*/
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+/*
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
+*/
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -116,6 +159,9 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 */
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
